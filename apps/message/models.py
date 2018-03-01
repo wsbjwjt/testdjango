f01d3ae2c3eda9837e3d8779ea7c01a1880fe34f
wsbjwjt <wsbjwjt@gmail.com>
2018-03-01 10:25:46 +0800

    version 2

--- apps/message/models.py
+++ apps/message/models.py
@@ -5,7 +5,8 @@ from django.db import models
 # Create your models here.
 
 # 继承于django.db.models.Model
-class userMessage(models.Model):
+class UserMessage(models.Model):
+
     object_id = models.CharField(primary_key=True, max_length=50, default="", verbose_name=u"主键")
     name = models.CharField(max_length=20, null=True, blank=True, verbose_name=u"用户名")
 
@@ -15,9 +16,11 @@ class userMessage(models.Model):
 
     message = models.CharField(max_length=500, verbose_name=u"留言板")
 
-
     class Meta:
         verbose_name = u"用户留言信息"
         # db_table ，这里我们让它自动生成所以不用指定
+
+        # db_table = "user_message"
+        # ordering = "-object_id"
 
         verbose_name_plural = verbose_name
--- apps/message/views.py
+++ apps/message/views.py
@@ -1,8 +1,47 @@
 # -*- encoding: UTF-8 -*-
 
 from django.shortcuts import render
+from .models import UserMessage
 
 # Create your views here.
 
 def getform(request):
+    # 方法一
+    # all_message = UserMessage.objects.all()
+    # all_message = UserMessage.objects.filter(name="wjt", address=u"北京")
+    # for message in all_message:
+        # print(message.name)
+
+    # user_message = UserMessage()
+
+    # user_message.name = "wjt"
+    # user_message.email = "wsbjwjt@163.com"
+    # user_message.address = "北京"
+    # user_message.message = "我爱北京天安门"
+    #
+    # user_message.save()
+
+    # 此处对应html中的method="post"，表示我们只处理post请求
+    # if request.method == "POST":
+    #     name = request.POST.get('name', '')
+    #     email = request.POST.get('email', '')
+    #     address = request.POST.get('address', '')
+    #     message = request.POST.get('message', '')
+    #
+    #     user_message = UserMessage()
+    #     user_message.name = name
+    #     user_message.address = address
+    #     user_message.email = email
+    #     user_message.message = message
+    #     user_message.object_id = 'ijkl'
+    #
+    #     user_message.save()
+
+    all_message = UserMessage.objects.filter(name='wjt', address='北京')
+
+    for message in all_message:
+
+        print(message.name)
+        all_message.delete()
+
     return render(request, 'message_form.html')
